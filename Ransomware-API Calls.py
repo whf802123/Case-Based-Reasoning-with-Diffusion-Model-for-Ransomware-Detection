f1c5abecde9d6443dcc26fe6dc349936830264ab
whf802123 <41955933+whf802123@users.noreply.github.com>
2026-06-07 11:51:13 +0800

    Update Ransomware-API Calls.py

--- Ransomware-API Calls.py
+++ Ransomware-API Calls.py
@@ -1,88 +1,119 @@
+
 import os
 import re
 import pandas as pd
 
-API_Calls = [
-    "CreateFile", "ReadFile", "WriteFile", "CloseHandle", "DeleteFile",
-    "GetFileAttributes", "SetFileAttributes", "FindFirstFile", "FindNextFile",
-    "CopyFile", "MoveFile", "OpenFile",
-    "RegOpenKey", "RegSetValue", "RegQueryValue", "RegCreateKey", "RegCloseKey",
-    "RegDeleteKey", "RegEnumKey", "RegEnumValue", "RegDeleteValue",
-    "WinHttpConnect", "WinHttpOpenRequest", "WinHttpSendRequest", "WinHttpReceiveResponse",
-    "send", "recv", "bind", "listen", "connect", "socket", "closesocket",
-    "VirtualAlloc", "VirtualFree", "GlobalAlloc", "GlobalFree", "HeapAlloc", "HeapFree",
-    "CreateProcess", "TerminateProcess", "GetExitCodeProcess", "OpenProcess",
-    "CreateThread", "ExitThread", "WaitForSingleObject", "WaitForMultipleObjects",
-    "TerminateThread", "ResumeThread", "SuspendThread",
-    "EnterCriticalSection", "LeaveCriticalSection",
-    "CreateMutex", "ReleaseMutex",
-    "CreateEvent", "SetEvent", "ResetEvent",
-    "CryptAcquireContext", "CryptEncrypt", "CryptDecrypt", "CryptGenKey", "CryptDestroyKey",
-    "OpenService", "StartService", "ControlService", "DeleteService", "CloseServiceHandle",
-    "FindWindow", "ShowWindow", "SetWindowText", "GetWindowText", "GetWindowRect",
-    "SetForegroundWindow", "GetLastError", "SetLastError",
-    "getFile", "write", "encrypt", "createWritable", "showDirectoryPicker",
-    "FileSystemFileHandle", "chooseFileSystemEntries",
-    "ReadDirectoryChangesW", "GetFileSize", "SetFilePointer", "FlushFileBuffers",
-    "LockFile", "UnlockFile", "BackupRead", "BackupWrite", "ReplaceFile",
-    "FindClose", "GetFileTime", "SetFileTime",
-    "RegFlushKey", "RegSaveKey", "RegLoadKey", "RegLoadMUIString",
-    "RegRestoreKey", "RegNotifyChangeKeyValue",
-    "WSAStartup", "WSACleanup", "WSASend", "WSARecv",
-    "InternetOpen", "InternetCloseHandle", "InternetReadFile", "InternetWriteFile",
-    "InternetSetOption", "HttpSendRequest", "HttpQueryInfo", "GetHostByName",
-    "GetHostByAddr", "GetAddrInfo", "FreeAddrInfo", "Socket",
-    "MapViewOfFile", "UnmapViewOfFile", "VirtualProtect", "VirtualQuery",
-    "GlobalLock", "GlobalUnlock", "LocalAlloc", "LocalFree", "GlobalReAlloc",
-    "VirtualLock", "VirtualUnlock", "RtlMoveMemory",
-    "ShellExecute", "ShellExecuteEx",
-    "GetProcessTimes", "GetThreadTimes",
-    "CreateRemoteThread", "OpenThread",
-    "SuspendProcess", "ResumeProcess",
-    "GetProcessId", "ExitProcess", "OpenJobObject",
-    "CreateSemaphore", "ReleaseSemaphore",
-    "SetWaitableTimer", "CancelWaitableTimer",
-    "InterlockedIncrement", "InterlockedDecrement", "PulseEvent",
-    "InitializeCriticalSectionAndSpinCount",
-    "CryptExportKey", "CryptImportKey", "CryptHashData", "CryptDeriveKey",
-    "EncryptFile", "DecryptFile", "CryptProtectData", "CryptUnprotectData",
-    "CryptHashSessionKey", "CryptSignHash", "CryptVerifySignature", "CryptSetKeyParam",
-    "MoveWindow", "SetWindowLong", "GetWindowLong",
-    "InvalidateRect", "RedrawWindow", "BringWindowToTop",
-    "CreateWindowEx", "GetWindowThreadProcessId",
-    "SendMessage", "PostMessage", "GetClassName", "AdjustWindowRect",
-    "GetSystemMetrics", "GetSystemTime", "SetSystemTime",
-    "QueryPerformanceCounter", "QueryPerformanceFrequency", "GetTickCount",
-    "GetDevicePowerState",
-    "FormatMessage", "SetUnhandledExceptionFilter", "RaiseException",
-    "CreatePipe", "CreateNamedPipe", "ConnectNamedPipe", "DisconnectNamedPipe",
-    "IsDebuggerPresent", "DebugActiveProcess", "DebugBreak"
-]
-
 dataset = "/Downloads/theZoo-master/malware/Source/Original"
+
+api_categories = {
+    "File Operation APIs": [
+        "CreateFile", "ReadFile", "WriteFile", "DeleteFile", "GetFileAttributes",
+        "SetFileAttributes", "FindFirstFile", "FindNextFile", "CopyFile", "MoveFile",
+        "OpenFile", "FlushFileBuffers", "LockFile", "UnlockFile", "BackupRead",
+        "BackupWrite", "ReplaceFile", "FindClose", "GetFileSize", "SetFilePointer",
+        "getFile", "write", "createWritable", "showDirectoryPicker",
+        "FileSystemFileHandle", "chooseFileSystemEntries"
+    ],
+    "Network Operation APIs": [
+        "send", "recv", "bind", "listen", "connect", "socket", "closesocket",
+        "WinHttpConnect", "WinHttpOpenRequest", "WinHttpSendRequest", "WinHttpReceiveResponse",
+        "WSAStartup", "WSACleanup", "WSASend", "WSARecv",
+        "InternetOpen", "InternetCloseHandle", "InternetReadFile", "InternetWriteFile",
+        "InternetSetOption", "HttpSendRequest", "HttpQueryInfo", "GetHostByName",
+        "GetHostByAddr", "GetAddrInfo", "FreeAddrInfo", "Socket"
+    ],
+    "Memory Management APIs": [
+        "VirtualAlloc", "VirtualFree", "HeapAlloc", "HeapFree",
+        "GlobalAlloc", "GlobalFree", "GlobalLock", "GlobalUnlock",
+        "LocalAlloc", "LocalFree", "GlobalReAlloc",
+        "VirtualLock", "VirtualUnlock", "RtlMoveMemory",
+        "MapViewOfFile", "UnmapViewOfFile", "VirtualProtect", "VirtualQuery"
+    ],
+    "Registry Operation APIs": [
+        "RegOpenKey", "RegSetValue", "RegQueryValue", "RegCreateKey", "RegCloseKey",
+        "RegDeleteKey", "RegEnumKey", "RegEnumValue", "RegDeleteValue",
+        "RegFlushKey", "RegSaveKey", "RegLoadKey", "RegLoadMUIString",
+        "RegRestoreKey", "RegNotifyChangeKeyValue"
+    ],
+    "Process and Thread Management APIs": [
+        "CreateProcess", "TerminateProcess", "GetExitCodeProcess", "OpenProcess",
+        "CreateThread", "ExitThread", "WaitForSingleObject", "WaitForMultipleObjects",
+        "TerminateThread", "ResumeThread", "SuspendThread",
+        "CreateRemoteThread", "OpenThread", "SuspendProcess", "ResumeProcess",
+        "GetProcessId", "ExitProcess", "OpenJobObject"
+    ],
+    "Encryption APIs": [
+        "CryptAcquireContext", "CryptEncrypt", "CryptDecrypt", "CryptGenKey", "CryptDestroyKey",
+        "CryptExportKey", "CryptImportKey", "CryptHashData", "CryptDeriveKey",
+        "EncryptFile", "DecryptFile", "CryptProtectData", "CryptUnprotectData",
+        "CryptHashSessionKey", "CryptSignHash", "CryptVerifySignature", "CryptSetKeyParam",
+        "encrypt"
+    ],
+    "GUI APIs": [
+        "FindWindow", "ShowWindow", "SetWindowText", "GetWindowText", "GetWindowRect",
+        "SetForegroundWindow", "MoveWindow", "SetWindowLong", "GetWindowLong",
+        "InvalidateRect", "RedrawWindow", "BringWindowToTop", "CreateWindowEx",
+        "GetWindowThreadProcessId", "SendMessage", "PostMessage", "GetClassName", "AdjustWindowRect"
+    ],
+    "System Management APIs": [
+        "GetSystemMetrics", "GetSystemTime", "SetSystemTime",
+        "QueryPerformanceCounter", "QueryPerformanceFrequency", "GetTickCount",
+        "GetDevicePowerState", "GetLastError", "SetLastError",
+        "FormatMessage", "SetUnhandledExceptionFilter", "RaiseException"
+    ],
+    "Pipe APIs": [
+        "CreatePipe", "CreateNamedPipe", "ConnectNamedPipe", "DisconnectNamedPipe"
+    ],
+    "Debug APIs": [
+        "IsDebuggerPresent", "DebugActiveProcess", "DebugBreak"
+    ]
+}
+
+all_apis = []
+for apis in api_categories.values():
+    for api in apis:
+        if api not in all_apis:
+            all_apis.append(api)
+
 results = []
 
-for i in os.listdir(dataset):
-    folder_path = os.path.join(dataset, i)
+for folder_name in os.listdir(dataset):
+    folder_path = os.path.join(dataset, folder_name)
 
     if os.path.isdir(folder_path):
-        count = {api: 0 for api in API_Calls}
+        api_count = {api: 0 for api in all_apis}
 
         for root, dirs, files in os.walk(folder_path):
             for file in files:
                 file_path = os.path.join(root, file)
+                try:
+                    with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
+                        content = f.read()
+                except:
+                    continue
+                for api in all_apis:
+                    api_count[api] += len(re.findall(r"\b" + re.escape(api) + r"\b", content))
 
-                with open(file_path, 'r', encoding="utf-8", errors="ignore") as f:
-                    content = f.read()
-                for api in API_Calls:
-                    count[api] += len(re.findall(api, content))
+        result = {"Ransomware": folder_name}
 
-        result = {'Ransomware': i}
-        result.update(count)
+        for category, apis in api_categories.items():
+            result[category + " Total"] = sum(api_count[api] for api in apis)
+            for api in apis:
+                result[api] = api_count[api]
+
         results.append(result)
 
-df = pd.DataFrame(results)
+columns = ["Ransomware"]
 
-output_file = "API Calls.xlsx"
+for category, apis in api_categories.items():
+    columns.append(category + " Total")
+    columns.extend(apis)
+
+df = pd.DataFrame(results)
+df = df[columns]
+
+output_file = "API_Categories_and_Individual_Counts.xlsx"
 df.to_excel(output_file, index=False)
+
+print(f"Results saved to {output_file}")
 
